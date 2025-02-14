477894fe08d8cb6bbe90077f48c7480dbd2eef84
Everson <esteveseverson@gmail.com>
2025-02-14 12:38:29 -0300

    1352

---
+++ 1352_product-last-k-numbers.py
@@ -0,0 +1,26 @@
+class ProductOfNumbers:
+
+    def __init__(self):
+        self.prefix_products = [1]
+        
+
+    def add(self, num: int) -> None:
+        if num == 0:
+            self.prefix_products = [1]
+        else:
+            self.prefix_products.append(self.prefix_products[-1] * num)
+
+    def getProduct(self, k: int) -> int:
+        if k >= len(self.prefix_products):
+            return 0
+        return self.prefix_products[-1] // self.prefix_products[-1 - k]
+
+
+productOfNumbers = ProductOfNumbers();
+productOfNumbers.add(1)
+print((productOfNumbers.getProduct(1)))
+print((productOfNumbers.getProduct(1)))
+print((productOfNumbers.getProduct(1)))
+productOfNumbers.add(7)
+productOfNumbers.add(6)
+productOfNumbers.add(7)
